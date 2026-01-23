58bda9f0a65c478390bd1baf54aad6ef58d7c4e8
admin <admin@10.208.36.162>
2026-01-23 12:34:04 +0530

    first commit

---
+++ cicddemomaven/src/main/java/com/cicd/CicddemomavenApplication.java
@@ -0,0 +1,13 @@
+package com.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicddemomavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicddemomavenApplication.class, args);
+	}
+
+}
