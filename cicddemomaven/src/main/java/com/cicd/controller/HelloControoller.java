78cc864248b5bb2b51a4e799ced065e1747bf266
admin <admin@10.208.36.162>
2026-01-27 14:46:15 +0530

    Third Commit

--- cicddemomaven/src/main/java/com/cicd/controller/HelloControoller.java
+++ cicddemomaven/src/main/java/com/cicd/controller/HelloControoller.java
@@ -1,5 +1,14 @@
 package com.cicd.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloControoller {
 
+	@GetMapping("/hello")
+	public String hello() {
+		System.out.println("Hello");
+		return "Hello";
+	}
 }
