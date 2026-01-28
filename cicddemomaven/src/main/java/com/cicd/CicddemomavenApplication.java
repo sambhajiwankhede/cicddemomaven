bec43b0b98ffcee319988d7e3cbeaa657cf9f7a9
admin <admin@10.208.36.162>
2026-01-28 16:01:54 +0530

    Final Commit

--- cicddemomaven/src/main/java/com/cicd/CicddemomavenApplication.java
+++ cicddemomaven/src/main/java/com/cicd/CicddemomavenApplication.java
@@ -2,12 +2,19 @@ package com.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicddemomavenApplication {
+public class CicddemomavenApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(CicddemomavenApplication.class, args);
-	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CicddemomavenApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicddemomavenApplication.class, args);
+    }
 
 }
